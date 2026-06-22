c2d03315cd93de1cec2f11fcb426e8d3ace811e3
Javigomez00 <javiergomez2001@gmail.com>
2026-06-22 20:55:08 -0300

    j

--- dashboard.php
+++ dashboard.php
@@ -376,9 +376,10 @@ $stat_mis_docs     = count(array_filter($mis_documentos, fn($d) => !empty($d)));
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal Educativo | Panel Principal</title>
-    <link rel="stylesheet" href="style/dashboard.css">
+    <link rel="stylesheet" href="Style/dashboard.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <link rel="icon" href="img/logo.png">
 </head>
 <body>
 
--- login.php
+++ login.php
@@ -55,6 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.png">
     <title>Login | Sistema de Gestión Educativa</title>
     <style>
         :root {
